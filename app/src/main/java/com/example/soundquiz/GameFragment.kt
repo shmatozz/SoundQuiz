8dc9feba016a9a2bef401fae9ad0034aba8e03f0
MaffyMaths <matvey33.baryshev@mail.ru>
2023-11-19 13:31:14 +0300

    fix player's name length, code for scores show

--- app/src/main/java/com/example/soundquiz/GameFragment.kt
+++ app/src/main/java/com/example/soundquiz/GameFragment.kt
@@ -1,18 +1,24 @@
 package com.example.soundquiz
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.soundquiz.databinding.FragmentGameBinding
 
 
 class GameFragment : Fragment() {
 
     private lateinit var binding: FragmentGameBinding
-    private lateinit var players: ArrayList<String>
+    private lateinit var players: ArrayList<Player>
     private var currentPlayer: Int = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,10 +27,55 @@ class GameFragment : Fragment() {
         binding = FragmentGameBinding.inflate(inflater, container, false)
         val root = binding.root
 
-        players = arguments?.getStringArrayList("players")!!
+        players = ArrayList()
+        for (name in arguments?.getStringArrayList("players")!!) {
+            players.add(Player(name, 0))
+        }
 
-        binding.currentPlayerTitle.text = getString(R.string.current_player, players[currentPlayer])
+        binding.currentPlayerTitle.text = getString(R.string.current_player, players[currentPlayer].name)
+
+        init()
 
         return root
+    }
+
+    private fun init() = binding.apply {
+        currentScoreButton.setOnClickListener {
+            showPlayersScores()
+        }
+
+        transportCard.setOnClickListener {
+
+        }
+
+        sportCard.setOnClickListener {
+
+        }
+
+        professionsCard.setOnClickListener {
+
+        }
+
+        natureCard.setOnClickListener {
+
+        }
+    }
+
+    private fun showPlayersScores() {
+        val scoresDialog = Dialog(requireContext())
+        scoresDialog.setContentView(R.layout.dialog_players_score)
+        scoresDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        scoresDialog.setCancelable(true)
+
+        val rc = scoresDialog.findViewById<RecyclerView>(R.id.players_rv)
+        rc.layoutManager = LinearLayoutManager(context)
+        val adapter = PlayerScoreAdapter()
+        rc.adapter = adapter
+
+        for (player in players) {
+            adapter.addPlayer(player)
+        }
+
+        scoresDialog.show()
     }
 }
