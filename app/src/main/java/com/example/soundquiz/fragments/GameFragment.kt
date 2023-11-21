ed716961ebeeb902c5f0c9b60289b2f807629b87
MaffyMaths <matvey33.baryshev@mail.ru>
2023-11-21 12:17:20 +0300

    add random word choosing after selecting card

--- app/src/main/java/com/example/soundquiz/fragments/GameFragment.kt
+++ app/src/main/java/com/example/soundquiz/fragments/GameFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -27,6 +28,13 @@ class GameFragment : Fragment() {
     private lateinit var players: ArrayList<Player>
     private var currentPlayer: Int = 0
 
+    private lateinit var transportWords: MutableList<String>
+    private lateinit var sportsWords: MutableList<String>
+    private lateinit var professionsWords: MutableList<String>
+    private lateinit var natureWords: MutableList<String>
+
+    private enum class Themes { TRANSPORT, SPORT, PROFESSION, NATURE }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,6 +47,11 @@ class GameFragment : Fragment() {
         for (name in arguments?.getStringArrayList("players")!!) {
             players.add(Player(name, 0))
         }
+
+        transportWords = getString(R.string.transport_words).split(',').toMutableList()
+        sportsWords = getString(R.string.sport_words).split(',').toMutableList()
+        professionsWords = getString(R.string.professions_words).split(',').toMutableList()
+        natureWords = getString(R.string.nature_words).split(',').toMutableList()
 
         binding.currentPlayerTitle.text = getString(R.string.current_player, players[currentPlayer].name)
 
@@ -53,23 +66,23 @@ class GameFragment : Fragment() {
         }
 
         transportCard.setOnClickListener {
-            cardSelected(1) // transport
+            cardSelected(Themes.TRANSPORT)
         }
 
         sportCard.setOnClickListener {
-            cardSelected(2) // sport
+            cardSelected(Themes.SPORT)
         }
 
         professionsCard.setOnClickListener {
-            cardSelected(3) // professions
+            cardSelected(Themes.PROFESSION)
         }
 
         natureCard.setOnClickListener {
-            cardSelected(4) // nature
+            cardSelected(Themes.NATURE)
         }
     }
 
-    private fun cardSelected(theme: Int) {
+    private fun cardSelected(theme: Themes) {
         val cardDialog = Dialog(requireContext())
         cardDialog.setContentView(R.layout.dialog_card_selected)
         cardDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
@@ -78,22 +91,25 @@ class GameFragment : Fragment() {
         val stopButton = cardDialog.findViewById<Button>(R.id.stop_button)
         val timer = cardDialog.findViewById<Chronometer>(R.id.timer)
         val card = cardDialog.findViewById<ConstraintLayout>(R.id.background)
+        val word = cardDialog.findViewById<TextView>(R.id.word)
 
         // set background according selected theme and generate word
         when (theme) {
-            1 -> {
+            Themes.TRANSPORT -> {
                 card.background = ContextCompat.getDrawable(requireContext(), R.drawable.card_blue)
+                word.text = getWord(Themes.TRANSPORT)
             }
-            2 -> {
+            Themes.SPORT -> {
                 card.background = ContextCompat.getDrawable(requireContext(), R.drawable.card_red)
+                word.text = getWord(Themes.SPORT)
             }
-            3 -> {
-                card.background = ContextCompat.getDrawable(requireContext(),
-                    R.drawable.card_yellow
-                )
+            Themes.PROFESSION -> {
+                card.background = ContextCompat.getDrawable(requireContext(), R.drawable.card_yellow)
+                word.text = getWord(Themes.PROFESSION)
             }
-            4 -> {
+            Themes.NATURE -> {
                 card.background = ContextCompat.getDrawable(requireContext(), R.drawable.card_green)
+                word.text = getWord(Themes.NATURE)
             }
         }
 
@@ -131,5 +147,50 @@ class GameFragment : Fragment() {
         }
 
         scoresDialog.show()
+    }
+
+    private fun getWord(theme: Themes) : String {
+        val word: String
+        when (theme) {
+            Themes.TRANSPORT -> {
+                word = transportWords.random()
+                transportWords.remove(word)
+            }
+            Themes.SPORT -> {
+                word = sportsWords.random()
+                sportsWords.remove(word)
+            }
+            Themes.PROFESSION -> {
+                word = professionsWords.random()
+                professionsWords.remove(word)
+            }
+            Themes.NATURE -> {
+                word = natureWords.random()
+                natureWords.remove(word)
+            }
+        }
+
+        checkAndDisableCard()
+
+        return word
+    }
+
+    private fun checkAndDisableCard() {
+        if (transportWords.size == 0) {
+            binding.transportCard.isEnabled = false
+            binding.transportCard.alpha = 0.4F
+        }
+        if (sportsWords.size == 0) {
+            binding.sportCard.isEnabled = false
+            binding.sportCard.alpha = 0.4F
+        }
+        if (professionsWords.size == 0) {
+            binding.professionsCard.isEnabled = false
+            binding.professionsCard.alpha = 0.4F
+        }
+        if (natureWords.size == 0) {
+            binding.natureCard.isEnabled = false
+            binding.natureCard.alpha = 0.4F
+        }
     }
 }
